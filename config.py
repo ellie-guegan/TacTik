c34bd5a76374800238b7a80cb117590039e97963
ellie-guegan <elisabeth_guegan@yahoo.co.uk>
2019-05-22 00:03:58 +0200

    Remove unused (#3)
    * removed the menu to choose which tokens to play with - they're all ugly anyway!
    
    * removed the ludo "end" section logic, now tokens just loop around the board indefinitely! yay.
    
    * fixed the starting coordinates to be the correct ones

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 
+import pygame
 
-# Function For Track Coordinates Calculations
+# ------------------------------------------
+# Track coordinates calculations
+# ------------------------------------------
 def rotateTrack(track):
     newTrack = []
     for i in track:
@@ -15,86 +18,48 @@ def rotateCoord(coord):
     newY = CENTER_Pos[1]+x
     return [newX, newY]
 
+# ------------------------------------------
+# Background hardcoded coordinates
+# ------------------------------------------
+
 CENTER_Pos = [330, 335]
 
 # 1 -> Yellow
-YELLOW_Pos = [
+YELLOW_Start = [550, 418]
+YELLOW_Start_Storage = [
+    [619, 352],
+    [619, 386],
+    [619, 418],
+    [619, 457],
+]
+YELLOW_End_Storage = [
     [503, 417],
     [503, 467],
     [503, 517],
     [503, 566],
 ]
-YELLOW_Start = [550, 418]
-YELLOW_Win = [
-    [334, 561],
-    [334, 523],
-    [334, 485],
-    [334, 447],
-    [334, 409],
-]
-YELLOW_End = [
-    [305, 370],
-    [325, 370],
-    [345, 370],
-    [365, 370],
-]
+
 YELLOW_Play = 0
 
 # 2 -> Blue
-BLUE_Pos = rotateTrack(YELLOW_Pos)
 BLUE_Start = rotateCoord(YELLOW_Start)
+BLUE_Start_Storage = rotateTrack(YELLOW_Start_Storage)
+BLUE_End_Storage = rotateTrack(YELLOW_End_Storage)
 
-BLUE_Win = [
-    [563, 333],
-    [525, 333],
-    [487, 333],
-    [449, 333],
-    [411, 333],
-]
-BLUE_End = [
-    [372, 347],
-    [372, 332],
-    [372, 317],
-    [372, 302],
-]
 BLUE_Play = 0
 
 # 3 -> Red
-RED_Pos = rotateTrack(BLUE_Pos)
 RED_Start = rotateCoord(BLUE_Start)
+RED_Start_Storage = rotateTrack(BLUE_Start_Storage)
+RED_End_Storage = rotateTrack(BLUE_End_Storage)
 
-RED_Win = [
-    [334, 104],
-    [334, 142],
-    [334, 180],
-    [334, 218],
-    [334, 256],
-]
-RED_End = [
-    [300, 285],
-    [320, 285],
-    [340, 285],
-    [372, 285],
-]
 RED_Play = 0
 
 # 4 -> Green
-GREEN_Pos = rotateTrack(RED_Pos)
 GREEN_Start = rotateCoord(RED_Start)
+GREEN_Start_Storage = rotateTrack(RED_Start_Storage)
+GREEN_End_Storage = rotateTrack(RED_End_Storage)
 
-GREEN_Win = [
-    [106, 333],
-    [144, 333],
-    [182, 333],
-    [220, 333],
-    [258, 333],
-]
-GREEN_End = [
-    [296, 355],
-    [296, 340],
-    [296, 325],
-    [296, 310],
-]
 GREEN_Play = 0
 
 YELLOW_TRACK = [
@@ -120,29 +85,28 @@ BLUE_TRACK = rotateTrack(YELLOW_TRACK)
 RED_TRACK = rotateTrack(BLUE_TRACK)
 GREEN_TRACK = rotateTrack(RED_TRACK)
 
-
 MAP = YELLOW_TRACK + BLUE_TRACK + RED_TRACK + GREEN_TRACK
 
 ITEMS_Pos = [
-    ["GREEN", GREEN_Pos[0][0], GREEN_Pos[0][1], False],
-    ["GREEN", GREEN_Pos[1][0], GREEN_Pos[1][1], False],
-    ["GREEN", GREEN_Pos[2][0], GREEN_Pos[2][1], False],
-    ["GREEN", GREEN_Pos[3][0], GREEN_Pos[3][1]],
+    ["GREEN", GREEN_Start_Storage[0][0], GREEN_Start_Storage[0][1], False],
+    ["GREEN", GREEN_Start_Storage[1][0], GREEN_Start_Storage[1][1], False],
+    ["GREEN", GREEN_Start_Storage[2][0], GREEN_Start_Storage[2][1], False],
+    ["GREEN", GREEN_Start_Storage[3][0], GREEN_Start_Storage[3][1], False],
 
-    ["RED", RED_Pos[0][0], RED_Pos[0][1], False],
-    ["RED", RED_Pos[1][0], RED_Pos[1][1], False],
-    ["RED", RED_Pos[2][0], RED_Pos[2][1], False],
-    ["RED", RED_Pos[3][0], RED_Pos[3][1], False],
+    ["RED", RED_Start_Storage[0][0], RED_Start_Storage[0][1], False],
+    ["RED", RED_Start_Storage[1][0], RED_Start_Storage[1][1], False],
+    ["RED", RED_Start_Storage[2][0], RED_Start_Storage[2][1], False],
+    ["RED", RED_Start_Storage[3][0], RED_Start_Storage[3][1], False],
 
-    ["BLUE", BLUE_Pos[0][0], BLUE_Pos[0][1], False],
-    ["BLUE", BLUE_Pos[1][0], BLUE_Pos[1][1], False],
-    ["BLUE", BLUE_Pos[2][0], BLUE_Pos[2][1], False],
-    ["BLUE", BLUE_Pos[3][0], BLUE_Pos[3][1], False],
+    ["BLUE", BLUE_Start_Storage[0][0], BLUE_Start_Storage[0][1], False],
+    ["BLUE", BLUE_Start_Storage[1][0], BLUE_Start_Storage[1][1], False],
+    ["BLUE", BLUE_Start_Storage[2][0], BLUE_Start_Storage[2][1], False],
+    ["BLUE", BLUE_Start_Storage[3][0], BLUE_Start_Storage[3][1], False],
 
-    ["YELLOW", YELLOW_Pos[0][0], YELLOW_Pos[0][1], False],
-    ["YELLOW", YELLOW_Pos[1][0], YELLOW_Pos[1][1], False],
-    ["YELLOW", YELLOW_Pos[2][0], YELLOW_Pos[2][1], False],
-    ["YELLOW", YELLOW_Pos[3][0], YELLOW_Pos[3][1], False],
+    ["YELLOW", YELLOW_Start_Storage[0][0], YELLOW_Start_Storage[0][1], False],
+    ["YELLOW", YELLOW_Start_Storage[1][0], YELLOW_Start_Storage[1][1], False],
+    ["YELLOW", YELLOW_Start_Storage[2][0], YELLOW_Start_Storage[2][1], False],
+    ["YELLOW", YELLOW_Start_Storage[3][0], YELLOW_Start_Storage[3][1], False],
 ]
 
 CurrentPos = -1
@@ -155,4 +119,21 @@ PList = [
     "YELLOW",
 ]
 
-    
+# ------------------------------------------
+# Token images
+# ------------------------------------------
+
+YELLOW_TOKEN_IMAGE = pygame.image.load("resources/img-ludo/pion_yellow0.png")
+BLUE_TOKEN_IMAGE = pygame.image.load("resources/img-ludo/pion_blue0.png")
+RED_TOKEN_IMAGE = pygame.image.load("resources/img-ludo/pion_red0.png")
+GREEN_TOKEN_IMAGE = pygame.image.load("resources/img-ludo/pion_green0.png")
+
+def getPlayerToken(color):
+    if (color == "GREEN"):
+        return GREEN_TOKEN_IMAGE
+    elif (color == "RED"):
+        return RED_TOKEN_IMAGE
+    elif (color == "BLUE"):
+        return BLUE_TOKEN_IMAGE
+    else:
+        return YELLOW_TOKEN_IMAGE
